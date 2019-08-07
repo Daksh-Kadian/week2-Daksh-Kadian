ada82ce230a86f9825809d25d38ff690172acd09
Daksh Kadian <daksh.kadian@niser.ac.in>
2019-08-07 19:43:48 +0530

    week 2 py27.py

---
+++ py27.py
@@ -0,0 +1,15 @@
+name = " John Wick "
+print (":"+name.lstrip()+":")
+
+
+print(":"+name.rstrip()+":")
+
+print(":"+name.strip()+":")
+
+name = "John \t Wick"
+print(name)
+
+
+name = "John \n Wick"
+print(name)
+
